b50638145f363ba66240316c54584b08c97c2ab3
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-11-11 14:48:11 +0700

    cấu hình lại file, tạo routes

--- index.js
+++ index.js
@@ -2,28 +2,15 @@ const express = require('express')
 const database = require('./config/database.js')
 require('dotenv').config()
 
+const routesApiVer1 = require('./api/v1/routes/index.route.js')
+
 const app = express()
 const port = process.env.PORT || 3000
 
 database.connect();
 
-const Task = require('./models/task.model.js');
-
-app.get('/task', async (req, res) => {
-  const tasks = await Task.find({ deleted: false })
-  res.json(tasks)
-})
-
-app.get('/task/detail/:id', async (req, res) => {
-  const id = req.params.id
-
-  const task = await Task.findOne({
-    _id: id,
-    deleted: false 
-  })
-  
-  res.json(task)
-})
+// Routes V1
+routesApiVer1(app)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
