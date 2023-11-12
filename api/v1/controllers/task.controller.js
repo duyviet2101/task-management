5a506220a4e9662bb1bd6eff8646e049666349e5
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-11-12 22:54:17 +0700

    sort

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -10,7 +10,15 @@ module.exports.index =  async (req, res) => {
     find.status = req.query.status
   }
 
+  //?sort
+  const sort = {}
+  if (req.query.sortKey && req.query.sortValue) {
+    sort[req.query.sortKey] = req.query.sortValue
+  }
+  //? end sort
+
   const tasks = await Task.find(find)
+    .sort(sort)
   res.json(tasks)
 }
 
